d9aece0adaf95463c50e7e023bec57b27ee2fd5b
Sebastian Raaphorst <sraaphorst@gmail.com>
2022-12-14 22:41:15 -1000

    Day13: Cleaning up Gradle build.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,11 +16,7 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
-    // These are needed for the Java ScriptEngine kts engine.
-    implementation("org.jetbrains.kotlin:kotlin-script-runtime")
-    implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable")
-    implementation("org.jetbrains.kotlin:kotlin-script-util")
-    implementation("org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable")
+    // Needed for the Java ScriptEngine kts engine. (Day 13)
     runtimeOnly("org.jetbrains.kotlin:kotlin-scripting-jsr223:1.7.20")
 }
 
